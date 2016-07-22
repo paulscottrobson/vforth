bae874dd80c82667d74db8e786c92463f09b8763
Paul Robson <paulscottrobson@gmail.com>
2016-07-22 08:00:09 +0100

    rejigged VM design and backend to be relocatable

--- ForthAsm/fasm.py
+++ ForthAsm/fasm.py
@@ -2,14 +2,16 @@ import sys
 from primitives import PrimitiveWord
 from vmbackend import VMBackEnd
 
-b = VMBackEnd()
+b = VMBackEnd(256)
 b.compileAllocate(2)
 b.compileLiteral(42)
 b.compileLiteral(-2)
 b.compileCall(514)
+b.compileCall(256)
+b.compilePrimitive(4)
 w = b.getHere()
 b.compileBranch(True)
-b.setBranchTarget(w,128)
-b.compilePrimitive(4)
-#b.compilePrimitive(27)
-ph = b.compileHeader("increment",123)
+b.setBranchTarget(w,w)
+b.compilePrimitive(27)
+ph = 256
+ph = b.compileHeader("increment",ph)
--- ForthAsm/vmbackend.py
+++ ForthAsm/vmbackend.py
@@ -6,7 +6,6 @@
 # **************************************************************************************************************************
 # **************************************************************************************************************************
 
-
 from primitives import PrimitiveWord
 
 # **************************************************************************************************************************
@@ -20,7 +19,9 @@ class VMBackEnd:
 		self.code = [] 																							# code words
 		self.isLastReturn = True 																				# true if last was return.
 		self.primitives = PrimitiveWord()																		# primitive information
-
+		self.opcodeBR = self.primitives.isPrimitive("$br")|0x80000000
+		self.opcode0BR = self.primitives.isPrimitive("$0br")|0x80000000
+		self.opcodeReturn = self.primitives.isPrimitive(";")|0x80000000
 	def getHere(self):
 		return self.pointer																						# current pointer
 
@@ -32,7 +33,14 @@ class VMBackEnd:
 		if not self.isLastReturn:																				# do we need it.
 			self.compileBranch(False)																			# compile branch w/o address
 		newHeader = self.getHere()																				# the new header pointer
-		self._compileWord(previousHeader)																		# address of previous.
+		self._compileWord(newHeader-previousHeader if previousHeader > 0 else 0)								# address of previous relative backwards if any.
+		self.compileIdentifier(name)
+		if not self.isLastReturn:																				# patch the branch over address
+			self.setBranchTarget(branchAddr,self.getHere())
+		self.isLastReturn = False																				# not previously a return now.
+		return newHeader																						# return next in chain.
+
+	def compileIdentifier(self,name):
 		currentWord = 0x0 																						# make word up.
 		for c in name:
 			if (currentWord & 0xFF000000) != 0:																	# if current word is full.
@@ -40,10 +48,6 @@ class VMBackEnd:
 				currentWord = 0																					# new word
 			currentWord = (currentWord << 8) | ord(c)															# add current character code in.
 		self._compileWord(currentWord)																			# compile the final word.
-		if not self.isLastReturn:																				# patch the branch over address
-			self.setBranchTarget(branchAddr,self.getHere())
-		self.isLastReturn = False																				# not previously a return now.
-		return newHeader																						# return next in chain.
 
 	def compileAllocate(self,count):
 		for i in range(0,count):																				# compile that many 0x00
@@ -54,30 +58,34 @@ class VMBackEnd:
 		literal = literal & 0xFFFFFFFF 																			# make 32 bit value
 		topBits = literal >> 30 														 						# look at top 2 bits.
 		assert topBits == 0 or topBits == 3,"Literal out of range" 												# must be 00x or 11x else can't sign extend
-		self._compileWord(literal | 0x80000000)																	# compile code for literal in.
+		self._compileWord(literal & 0x7FFFFFFF)																	# compile code for literal in.
 		self.isLastReturn = False
 
 	def compileCall(self,address): 		
-		assert address >= 0x100 and address < 0x80000000,"Bad call address"										# check legitimate value.
-		self._compileWord(address)																				# compile call as just address
+		offset = address-(self.getHere()+4)
+		if offset < 0:
+			self._compileWord(0x90000000 - offset)
+		else:
+			self._compileWord(0xA0000000 + offset)
 		self.isLastReturn = False
 
 	def compilePrimitive(self,primitive):	
 		assert self.primitives.getPrimitive(primitive) is not None,"Unknown primitive "+str(primitive)			# check legit
+		primitive |= 0x80000000
 		self._compileWord(primitive)																			# compile primitive
-		self.isLastReturn = self.primitives.getPrimitive(primitive) == ";"										# record if last was ;
+		self.isLastReturn = (primitive == self.opcodeReturn)													# record if last was ;
 
 	def compileBranch(self,isConditional):	
-		self._compileWord(self.primitives.isPrimitive("$0br" if isConditional else "$br"))						# compile $0br or $br
+		self._compileWord(self.opcode0BR if isConditional else self.opcodeBR)									# compile $0br or $br
 		self._compileWord(0)																					# no address yet
 		self.isLastReturn = False
 
 	def setBranchTarget(self,branchAddress,targetAddress):
 		addr = (branchAddress - self.base)/4																	# convert to index in word array
 		op = self.code[addr] 																					# check op is $0BR or $BR
-		assert op == self.primitives.isPrimitive("$br") or op == self.primitives.isPrimitive("$0br")
-		self.code[addr+1] = targetAddress 																		# update target address
-		print("{0:08x} {1:08x} {1} PATCH".format(branchAddress+4,targetAddress))
+		assert op == self.opcode0BR or op == self.opcodeBR
+		self.code[addr+1] = (targetAddress - (branchAddress + 8)) & 0xFFFFFFFF									# update target address
+		print("{0:08x} {1:08x} Rel Patch {2:08x}".format(branchAddress+4,self.code[addr+1],targetAddress))
 
 	def _compileWord(self,word):
 		print("{0:08x} {1:08x} {2}".format(self.pointer,word,self.decode(word)))								# output word
@@ -86,14 +94,17 @@ class VMBackEnd:
 
 	def decode(self,word):
 		rv = "?"																								# lazy convert back to mnemonic code.
-		if (word & 0x80000000) != 0:
+		if (word & 0x80000000) == 0:
 			if (word & 0x40000000) == 0:
 				word = word & 0x3FFFFFFF
 			else:
 				word = (word & 0x7FFFFFFF) - 0x80000000
 			rv = str(word)
-		elif word < 256:
-			rv = self.primitives.getPrimitive(word)
+		elif (word >> 28) == 8:
+			rv = self.primitives.getPrimitive(word & 0xFF)
 		else:
-			rv = "call "+str(word)
+			offset = word & 0x0FFFFFFF
+			if (word >> 28) == 9:
+				offset = -offset
+			rv = "call {0:08x}".format(offset)
 		return rv
